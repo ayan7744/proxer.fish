c082a80098bf3a3f4c0a6362539a9a789b57e619
Ayan Nath <ayannath7744@gmail.com>
2021-05-04 12:55:51 +0530

    Upload progress

--- set_proxy.sh
+++ set_proxy.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 # Script to set system-wide proxy in Arch
 
+# if IS_PROXY_SET env var doesn't exist, then export it.
+[ -z "$IS_PROXY_SET" ] && export IS_PROXY_SET=0
+
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    configFile="$HOME/.config/proxer/proxy"
+else 
+    configFile="$XDG_CONFIG_HOME/proxer/proxy"
+fi
+
+configFileHead="$(/bin/cat $configFile | head -1)"
+connectedWifi="$(nmcli -t -f NAME connection show --active)"
+ssid="$(echo "$configFileHead" | cut -d' ' -f1)"
+proxyHost="$(echo "$configFileHead" | cut -d' ' -f2)"
+proxyPort="$(echo "$configFileHead" | cut -d' ' -f3)"
+proxyUser="$(echo "$configFileHead" | cut -d' ' -f4)"
+proxyPass="$(echo "$configFileHead" | cut -d' ' -f5)"
+
+if [ -z "$proxyUser" ]; then 
+    proxyServer="http://$proxyHost:$proxyPort/"
+elif [ -z "$proxyPass" ]; then 
+    proxyServer="http://$proxyUser@$proxyHost:$proxyPort/"
+else 
+    proxyServer="http://$proxyUser:$proxyPass@$proxyHost:$proxyPort/"
+fi
+
 check_su() {
     if [ `id -u` -ne 0 ]; then 
         echo "Must be run with root privileges. Exiting..."
@@ -10,43 +35,25 @@ check_su() {
 
 _exit() {
     echo "proxer: invalid option"
-    echo "Usage: ./set_proxy.sh -h [PROXY HOST] -p [PROXY PORT]"
-    echo "Try './set_proxy.sh --help' for more information."
+    echo "Usage: proxer -h [PROXY HOST] -p [PROXY PORT]"
+    echo "Try 'proxer --help' for more information."
     # Help to be added later when script functions increases
     exit 1
-}
-
-reset_proxy () {
-    gsettings set org.gnome.system.proxy mode none
-    truncate -s 0 /etc/profile.d/proxy.sh
-    sed -i.bak "/Acquire::/d" /etc/apt/apt.conf
-    sed -i.bak "/Acquire::/,+10d" /etc/apt/apt.conf.d/70debconf
-    sed -i "/proxy/d" /etc/environment
-    sed -i "/PROXY/d" /etc/environment
-    if hash git 2>/dev/null; then
-      git config --global --unset http.proxy
-      git config --global --unset https.proxy
-    fi
-    if [ -a /etc/systemd/system/docker.service.d/proxy.conf ]; then
-      sudo rm -rf /etc/systemd/system/docker.service.d/proxy.conf
-    fi
-    sudo systemctl daemon-reload
-    sudo systemctl restart docker.service
 }
 
 # setting system wide proxy
 gsettings_proxy () {
     if [ "$1" = "--set" ]; then 
         gsettings set org.gnome.system.proxy mode 'manual'
-        gsettings set org.gnome.system.proxy.http host "$PROXY_HOST"
-        gsettings set org.gnome.system.proxy.http port "$PROXY_PORT"
-        gsettings set org.gnome.system.proxy.https host "$PROXY_HOST"
-        gsettings set org.gnome.system.proxy.https port "$PROXY_PORT"
-        gsettings set org.gnome.system.proxy.ftp host "$PROXY_HOST"
-        gsettings set org.gnome.system.proxy.ftp port "$PROXY_PORT"
+        gsettings set org.gnome.system.proxy.http host "$2"
+        gsettings set org.gnome.system.proxy.http port "$3"
+        gsettings set org.gnome.system.proxy.https host "$2"
+        gsettings set org.gnome.system.proxy.https port "$3"
+        gsettings set org.gnome.system.proxy.ftp host "$2"
+        gsettings set org.gnome.system.proxy.ftp port "$3"
         gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.1', 'localaddress','.localdomain.com', '::1', '10.*.*.*']"
     elif [ "$1" = "--unset" ]; then
-        [ "$IS_PROXY_SET" = "0" ] && return 0
+        gsettings set org.gnome.system.proxy mode none
     else 
         exit 1
     fi 
@@ -61,32 +68,68 @@ apt_proxy () {
 # setting environment variables
 env_proxy () { 
     if [ "$1" = "--set" ]; then 
-        [ "$IS_PROXY_SET" = "1" ] && return 0   
-        export http_proxy=$argv[1]
-        export HTTP_PROXY=$http_proxy
-        export https_proxy=$http_proxy
-        export HTTPS_PROXY=$http_proxy
-        export ftp_proxy=$http_proxy
-        export FTP_PROXY=$http_proxy
-        export rsync_proxy=$http_proxy
-        export RSYNC_PROXY=$http_proxy
-        export all_proxy=$http_proxy
+        export http_proxy="$2"
+        export HTTP_PROXY="$http_proxy"
+        export https_proxy="$http_proxy"
+        export HTTPS_PROXY="$http_proxy"
+        export ftp_proxy="$http_proxy"
+        export FTP_PROXY="$http_proxy"
+        export rsync_proxy="$http_proxy"
+        export RSYNC_PROXY="$http_proxy"
+        export all_proxy="$http_proxy"
         export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
-        export IS_PROXY_ON=1
     elif [ "$1" = "--unset" ]; then
-        [ "$IS_PROXY_SET" = "0" ] && return 0
-        
+        unset http_proxy HTTP_PROXY https_proxy HTTPS_PROXY ftp_proxy FTP_PROXY rsync_proxy RSYNC_PROXY all_proxy no_proxy
     else 
         exit 1
     fi 
 }
 
-# set git proxy
-set_git_proxy () {
-    if hash git 2>/dev/null; then
-      git config --global http.proxy "http://${PROXY_HOST}:${PROXY_PORT}"
-      git config --global https.proxy "http://${PROXY_HOST}:${PROXY_PORT}"
+# manage git proxy
+git_proxy () {
+    if [ "$1" = "--set" ]; then 
+        if hash git 2>/dev/null; then
+          git config --global http.proxy "$2"
+          git config --global https.proxy "$2"
+        fi
+    elif [ "$1" = "--unset" ]; then
+        if hash git 2>/dev/null; then
+          git config --global --unset http.proxy
+          git config --global --unset https.proxy
+        fi
+    else 
+        exit 1
+    fi 
+}
+
+set_all_proxy() {
+    [ "$IS_PROXY_SET" = "1" ] && return 0   
+    gsettings_proxy --set "$proxyHost" "$proxyPort"
+    git_proxy --set "$proxyServer"
+    env_proxy --set "$proxyServer"
+    export IS_PROXY_SET=1
+}
+
+unset_all_proxy() {
+    [ "$IS_PROXY_SET" = "0" ] && return 0   
+    gsettings_proxy --unset
+    git_proxy --unset
+    env_proxy --unset
+    export IS_PROXY_SET=0
+}
+
+auto_proxy() {
+    if [ "$connectedWifi" = "$ssid" ]
+        set_proxy "$proxyServer"
+    else
+        unset_proxy
     fi
+}
+
+reset_proxy() {
+    unset_all_proxy
+    sed -i.bak "/Acquire::/d" /etc/apt/apt.conf
+    sed -i.bak "/Acquire::/,+10d" /etc/apt/apt.conf.d/70debconf
 }
 
 if [ "$#" -eq 1 ]; then
